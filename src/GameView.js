852d650dc6a4812986383e172e3cfb6ddcffe78a
Danny Rivers <cdeevfrr@gmail.com>
2021-10-06 21:40:43 -0700

    Make a simple gameView skeleton

---
+++ src/GameView.js
@@ -0,0 +1,11 @@
+function GameView({tiles, entities}){
+    return (
+        <div>
+            <p>This will be the game.
+                I was passed tiles {tiles}
+                and entities {entities}
+            </p>
+        </div>)
+}
+
+export default GameView
